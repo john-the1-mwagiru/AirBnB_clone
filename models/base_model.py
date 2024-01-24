4c030699b8ce9ca9cd18b858d33fe38814ff1c53
john-the1-mwagiru <johnmwagiru199@gmail.com>
2024-01-24 11:32:40 +0300

    task-0:I recreated an instance with the dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -2,16 +2,24 @@ from uuid import uuid4
 from datetime import datetime
 """BaseModel class that defines all common attributes/methods for other clasess"""
 class BaseModel:
+  
     """Represents the BaseModel for Airbnb project"""
     def __init__(self,*args, **kwargs):
         """Initialize a new BaseModel"""
         self.id = str(uuid4())
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for name, value in kwargs.items():
+                if name =="created_at" or name == "updated_at":
+                    self.__dict__[name] =datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[name] = value
     def __str__(self):
         """Returns a string representation of the BaseModel instance"""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+  
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
@@ -20,7 +28,7 @@ class BaseModel:
         """Returns a dictionary containing all key/values of __dict__"""
         return{
             "id":self.id,
-            "created_at":self.created_at,
-            "updated_at":self.updated_at,
+            "created_at":self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+            "updated_at":self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
             "__class__": self.__class__.__name__
         }
